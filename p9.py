9075dc804fcafba0a3ff1daebaed0f3a453ff1c2
Saathwik123 <112624981+Saathwik123@users.noreply.github.com>
2022-09-05 13:41:50 +0530

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,11 @@
+#Addition
+add = 5 + 2
+print ("Addition: %d" %add)
+ 
+#Subtraction
+sub = 5 - 2
+print ("Subtraction: %d" %sub)
+ 
+#Multiplication
+mul = 5 * 2
+print ("Multiplication: %d" %mul)
